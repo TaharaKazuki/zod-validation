7e0a6ca1e110d625e07f53ac8fce3ecf9dddef57
Tahara Kazuki <ssp0727.lnc@gmail.com>
2023-02-07 15:19:50 +0900

    update

--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -1,7 +1,25 @@
-export default function SignUpPage () {
-    return (
-        <div>
-            SignUpForm Here
-        </div>
-    )
+import { Input } from 'react-daisyui'
+
+export default function SignUpPage() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexFlow: 'column',
+        gap: 15,
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+      }}
+    >
+      SignUpForm Here
+      <div className="form-control w-full max-w-xs">
+        <label htmlFor={'email'} className="label">
+          <span className="label-text">email</span>
+        </label>
+        <Input color={'ghost'} id={'email'} type={'text'} />
+        <span className="label-text text-error">An error</span>
+      </div>
+    </div>
+  )
 }
